033f90aaf977a1a5170af92bc17401d24eb00727
werido <359066432@qq.com>
2023-01-12 17:45:15 +0800

    🎉 init: update

--- flask_test/conf.py
+++ flask_test/conf.py
@@ -1,3 +1,7 @@
+import environ,os
+environ.Env.read_env(os.path.join(os.path.dirname(__file__),".env"))
+env = environ.Env()
+
 CELERY_BROKER_URL =  f"redis://:werido@8.131.78.84:6379/1"
 CELERY_RESULT_BACKEND = CELERY_BROKER_URL
 #: Only add pickle to this list if your broker is secured
